7f1d758abd1b2bd6b72d6684f3cc2b13c64843ad
Tez <38469636+TezSmith@users.noreply.github.com>
2018-12-17 18:23:52 -0500

    Starter Files

---
+++ run.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require_relative './guest'
+require_relative './listing'
+require_relative './trip'
+
+#write your testing code & variables here!
+
+
+
+
+
+
+binding.pry
---
+++ trip.rb
@@ -0,0 +1,3 @@
+class Trip
+
+end 
